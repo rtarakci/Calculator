4ed62dc6862c9c444aea444624ae04e81ac3e99a
rtarakci <47783552+rtarakci@users.noreply.github.com>
2019-11-27 14:16:46 +0100

    defensive programmierung

--- src/main/java/Tarakci/Ridvan/Calculator/CalculatorApplication.java
+++ src/main/java/Tarakci/Ridvan/Calculator/CalculatorApplication.java
@@ -10,13 +10,29 @@ public class CalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
+        boolean x1Number = false, x2Number = false;
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Eingabe 1:");
-        double x1 = sc.nextDouble();
-        System.out.print("Eingabe 2: ");
-        double x2 = sc.nextDouble();
+        double x1 = 0 , x2 = 0;
+        do try {
+            System.out.print("Input 1:");
+            x1 = Double.parseDouble(sc.next());
+            x1Number = true;
+        } catch (Exception e) {
+            System.out.println("Not a number, try again");
+        } while(!x1Number);
+        do {
 
+try {
+
+    System.out.println();
+    System.out.print("Input 2: ");
+    x2 = Double.parseDouble(sc.next());
+    x2Number = true;
+}catch (Exception e){
+    System.out.println("Not a number, try again");
+}
+        }while(!x2Number);
         System.out.println("Ergebnis: " + ((x1+x2)/(x1*x2)));
 
     }
